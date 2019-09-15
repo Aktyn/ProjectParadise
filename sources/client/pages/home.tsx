caa759b09b11c10533dd8aa1de58629658d1fc7d
Aktyn <Aktyn3@gmail.com>
2019-09-15 20:25:55 +0200

    added links in homepage

--- sources/client/pages/home.tsx
+++ sources/client/pages/home.tsx
@@ -1,8 +1,25 @@
 import * as React from 'react';
+import {Link} from 'react-router-dom';
 import {onceHasInstantInviteLink} from "../discord_info";
 // import CloudBtn from "../components/cloud_btn";
 
 import '../styles/home.scss';
+
+const ShinyLink = (props: {content: string, href: string, target?: string, separate_page?: boolean}) => {
+	let inside = <>
+		<span>{props.content}</span>
+		<div className={'left'} />
+		<div className={'right'} />
+		<div className={'top'} />
+		<div className={'bottom'} />
+	</>;
+	if(props.separate_page)
+		return <a href={props.href} target={props.target || '_self'}>
+			{inside}
+		</a>;
+	else
+		return <Link to={props.href}>{inside}</Link>;
+};
 
 interface HomeState {
 	discord_invitation_link?: string;
@@ -20,11 +37,13 @@ export default class Home extends React.Component<any, HomeState> {
 		return <div className={'home'}>
 			{/*<CloudBtn width={256} height={256} content={'DISCORD'} />*/}
 			<section className={'links'}>
+				<ShinyLink content={'FORUM'} href={'/forum'} separate_page={true} />
 				{this.state.discord_invitation_link &&
-					<a href={this.state.discord_invitation_link} target={'_blank'}>
-						<div className={'water-text'} data-text={'DISCORD'}>DISCORD</div>
-					</a>
+					<ShinyLink content={'DISCORD'} href={this.state.discord_invitation_link} target={'_blank'}
+						separate_page={true} />
 				}
+				<ShinyLink content={'REGULAMIN'} href={'/regulamin'} />
+				<ShinyLink content={'KLAWISZOLOGIA'} href={'/klawiszologia'} />
 			</section>
 		</div>;
 	}
