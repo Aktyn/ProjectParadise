9fad353033ea0736052a10aa97a7bbe0c423bbab
Aktyn <Aktyn3@gmail.com>
2019-09-16 00:53:41 +0200

    online players list

--- sources/client/components/footer.tsx
+++ sources/client/components/footer.tsx
@@ -2,12 +2,23 @@ import * as React from 'react';
 import '../styles/footer.scss';
 import {trimString} from "../utils";
 import {DiscordMember, offDiscordMembersUpdate, onDiscordMembersUpdate} from "../discord_info";
+import Config from "../../common/config";
 
 const github_link = 'https://github.com/Aktyn';
+
+interface PlayerSchema {
+	endpoint: string;
+	id: number;
+	identifiers: string[];
+	name: string;
+	ping: number;
+}
 
 interface FooterState {
 	author_info: boolean;
 	members: DiscordMember[];
+	online_players: PlayerSchema[];
+	open_players_list: boolean;
 }
 
 export default class Footer extends React.Component<any, FooterState> {
@@ -15,14 +26,35 @@ export default class Footer extends React.Component<any, FooterState> {
 	
 	state: FooterState = {
 		author_info: false,
-		members: []
+		members: [],
+		online_players: [],
+		open_players_list: false
 	};
 	
 	componentDidMount() {
 		onDiscordMembersUpdate(this.updateMembers);
+		this.loadOnlinePlayers();
 	}
 	componentWillUnmount() {
 		offDiscordMembersUpdate(this.updateMembers);
+	}
+	
+	private loadOnlinePlayers() {
+		fetch(`https://cors-anywhere.herokuapp.com/${Config.FIVEM_URL}/players.json`, {
+			method: 'GET',
+			mode: 'cors',
+			headers: {"Content-Type": "application/json; charset=utf-8"},
+			cache: 'reload'
+		}).then(res => res.json()).then((res: PlayerSchema[]) => {
+			this.setState({online_players: res});
+			
+			/*res = JSON.parse('[{"endpoint":"127.0.0.1","id":1,"identifiers":["steam:11000010d695d4e","license:d3fa8fbd2026b2ad7429be7df1ede2c7eaf118d0","discord:463821661637050389"],"name":"Magnum44","ping":70}]');
+			this.setState({
+				online_players: [...res, ...res, ...res, ...res, ...res, ...res, ...res, ]
+			});*/
+		}).catch(console.error);
+		
+		setTimeout(() => this.loadOnlinePlayers(), 1000*60);//update every minute
 	}
 	
 	private static renderAuthorInfo() {
@@ -51,8 +83,32 @@ export default class Footer extends React.Component<any, FooterState> {
 	}
 	
 	render() {
+		let online_count = this.state.online_players.length;
 		return <footer>
-			<div className={'footer-top'} />
+			<div className={'footer-top'}>
+				<div className={'online-info'}>{online_count === 0 ? <span>Brak graczy na serwerze</span> :
+					<span>Na serwerze {online_count === 1 ? 'gra aktualnie jedna osoba.' : <>
+						{online_count < 5 ?
+							<>grają aktualnie {online_count} osoby.</> : <>gra aktualnie {online_count} osób.</>}
+					</>}</span>
+				}
+					{online_count > 0 && <button className={`show-list-btn${
+						this.state.open_players_list ? ' close' : ''}`} onClick={() =>
+					{
+						this.setState({open_players_list: !this.state.open_players_list})
+					}}/>}
+					<div className={'online-list'} style={{
+						height: this.state.open_players_list ?
+							`${15*(1+Math.min(10, this.state.online_players.length))}px` : '0px'
+					}}><div className={'scroll-area'}>
+						<table><tbody>{this.state.online_players.map(player => {
+							return <tr key={player.id}>
+								<td>{player.name}</td>
+							</tr>;
+						})}</tbody></table>
+					</div></div>
+				</div>
+			</div>
 			<section className={'footer-content'}>
 				<div className={'moon-widget'}>
 					{this.state.author_info ?
--- sources/common/config.ts
+++ sources/common/config.ts
@@ -1,5 +1,6 @@
 
 const Config = {
-	SERVER_PORT: 1234
+	SERVER_PORT: 1234,
+	FIVEM_URL: 'http://51.91.156.245:30120'
 };
 export default Config;
